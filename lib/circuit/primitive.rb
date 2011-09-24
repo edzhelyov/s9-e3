90a6801ffd11e08005970e4b92ae70cbe20513dc
Evgeni Dzhelyov <evgeni.dzhelyov@gmail.com>
2011-09-24 20:10:44 +0300

    Add Element object in the javascript

--- lib/circuit/primitive.rb
+++ lib/circuit/primitive.rb
@@ -1,13 +1,13 @@
 module Circuit
   class Primitive
     def to_svg
-      '<svg contentScriptType="text/ecmascript" width="40" xmlns:xlink="http://www.w3.org/1999/xlink" zoomAndPan="magnify" contentStyleType="text/css" height="40" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg" version="1.0"><g><rect fill="red" x="0" y="0" width="40" height="40" /><circle class="input" cx="5" cy="5" r="3" /><circle class="input" cx="5" cy="35" r="3" /><circle class="output" cx="35" cy="20" r="3" /></g></svg>'
+      '<svg contentScriptType="text/ecmascript" width="40" xmlns:xlink="http://www.w3.org/1999/xlink" zoomAndPan="magnify" contentStyleType="text/css" height="40" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg" version="1.0"><g data-type="XOR"><rect fill="red" x="0" y="0" width="40" height="40" /><circle class="input" cx="5" cy="5" r="3" /><circle class="input" cx="5" cy="35" r="3" /><circle class="output" cx="35" cy="20" r="3" /></g></svg>'
     end
   end
 
   class GreePrimitive
     def to_svg
-      '<svg contentScriptType="text/ecmascript" width="40" xmlns:xlink="http://www.w3.org/1999/xlink" zoomAndPan="magnify" contentStyleType="text/css" height="40" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg" version="1.0"><g><rect fill="green" x="0" y="0" width="40" height="40" /><circle class="input" cx="5" cy="5" r="3" /><circle class="input" cx="5" cy="35" r="3" /><circle class="output" cx="35" cy="20" r="3" /></g></svg>'
+      '<svg contentScriptType="text/ecmascript" width="40" xmlns:xlink="http://www.w3.org/1999/xlink" zoomAndPan="magnify" contentStyleType="text/css" height="40" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg" version="1.0"><g data-type="AND"><rect fill="green" x="0" y="0" width="40" height="40" /><circle class="input" cx="5" cy="5" r="3" /><circle class="input" cx="5" cy="35" r="3" /><circle class="output" cx="35" cy="20" r="3" /></g></svg>'
     end
   end
 end
