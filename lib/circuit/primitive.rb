c26c4d0fab1d736c2ad416ba23a8fe12ab52a0ad
Evgeni Dzhelyov <evgeni.dzhelyov@gmail.com>
2011-09-25 15:02:24 +0300

    Handle dynamic svg building into the Primitive itself

--- lib/circuit/primitive.rb
+++ lib/circuit/primitive.rb
@@ -1,32 +1,38 @@
 module Circuit
   class Primitive
-    attr_accessor :id, :type, :inputs, :output, :color
+    attr_accessor :type, :inputs, :color
 
     def self.factory(type, options = {})
       Circuit.const_get(type.to_s).new(options)
     end
 
     def initialize(options = {})
-      @id = options['id']
       @type = options['type']
-      @inputs = options['inputs']
-      @output = options['output']
+      @inputs = options['inputs'] || [OFF, OFF]
       @color = 'red'
     end
 
-    def to_svg
+    def svg
+      # Fix naming collision with Batik's own #color
       type = @type
       color = @color
+      input_1 = inputs[0].execute ? 'yellow' : 'black'
+      input_2 = inputs[1].execute ? 'yellow' : 'black'
+      output = execute ? 'yellow' : 'black'
 
       Batik::SVG.new(:width => 40, :height => 40) do
         group('data-type' => type, 'font-size' => 8) do
           rectangle(:fill => color, :x => 0, :y => 0, :width => 40, :height => 40)
-          circle(:class => 'input', :cx => 5, :cy => 5, :r => 3, 'data-source' => 'off')
-          circle(:class => 'input', :cx => 5, :cy => 35, :r => 3, 'data-source' => 'off')
-          circle(:class => 'output', :cx => 35, :cy => 20, :r => 3, 'data-source' => 'off')
+          circle(:class => 'input', :cx => 5, :cy => 5, :r => 3, 'data-source' => 'off', :fill => input_1)
+          circle(:class => 'input', :cx => 5, :cy => 35, :r => 3, 'data-source' => 'off', :fill => input_2)
+          circle(:class => 'output', :cx => 35, :cy => 20, :r => 3, 'data-source' => 'off', :fill => output)
           text(:x => 10, :y => 22, :body => type)
         end
-      end.to_s
+      end
+    end
+
+    def to_svg
+      svg.to_s
     end
   end
 
